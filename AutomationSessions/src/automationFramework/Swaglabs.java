957677528e531e429ef31448163a0b2ad6e38cf7
Admin <Admin@RajashekarReddy>
2020-12-07 09:03:58 +0530

    Added

--- AutomationSessions/src/automationFramework/Swaglabs.java
+++ AutomationSessions/src/automationFramework/Swaglabs.java
@@ -15,6 +15,7 @@ public class Swaglabs {
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 		driver.findElement(By.id("login-button")).click();
+		
 	}
 
 }
